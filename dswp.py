1cb444971e450be966396afb8e799acadb071755
Sedinei <sedinei@hotmail.com>
2021-01-10 17:48:57 -0300

    Inclusão de novas funções do curso

--- dswp.py
+++ dswp.py
@@ -8,6 +8,7 @@ import numpy as np
 import pandas as pd
 from scipy import stats
 from sklearn.feature_selection import SelectFromModel
+from sklearn.cluster import KMeans
 
 def seleciona_colunas_relevantes(modelo, X_treinamento, X_teste, threshold = 0.05):
     # Cria um seletor para selecionar as COLUNAS com importância > threshold
@@ -361,3 +362,61 @@ def trata_colunas_numericas(df):
         trata_missing_values(df, coluna)
         aplica_MMS(df, coluna)
         aplica_RS(df, coluna)
+
+# Função adaptada de: https://www.geeksforgeeks.org/elbow-method-for-optimal-value-of-k-in-kmeans/
+def Numero_Clusters_Elbow(X):
+    distortions = [] 
+    inertias = [] 
+    mapping1 = {} 
+    mapping2 = {} 
+    K = range(1,10)    
+    for k in K:
+        #Building and fitting the model 
+        kmeanModel = KMeans(n_clusters=k).fit(X) 
+        kmeanModel.fit(X)
+        distortions.append(sum(np.min(cdist(X, kmeanModel.cluster_centers_, 'euclidean'),axis=1)) / X.shape[0]) 
+        inertias.append(kmeanModel.inertia_)
+        mapping1[k] = sum(np.min(cdist(X, kmeanModel.cluster_centers_, 'euclidean'),axis=1)) / X.shape[0] 
+        mapping2[k] = kmeanModel.inertia_ 
+
+    # Using the different values of Distortion
+    print('Cálculo da Distorção:')
+    for key,val in mapping1.items():
+        print(str(key)+' : '+str(val))
+
+    plt.plot(K, distortions, 'bx-')
+    plt.xlabel('Values of K')
+    plt.ylabel('Distortion')
+    plt.title('The Elbow Method using Distortion')
+    plt.show() 
+
+    # Using the different values of Inertia
+    print('Cálculo da Inertia:')
+    for key,val in mapping2.items():
+        print(str(key)+' : '+str(val))
+
+    plt.plot(K, inertias, 'bx-')
+    plt.xlabel('Values of K')
+    plt.ylabel('Inertia') 
+    plt.title('The Elbow Method using Inertia')
+    plt.show() 
+
+def importancia_variaveis():
+    # Calcula a importância das features
+    importances = ml_rf.feature_importances_
+
+    # Ordena as features por importância
+    indices = np.argsort(importances)[::-1]
+
+    # Associa a feature name com a feature importance
+    names = [iris.feature_names[i] for i in indices]
+
+    # Barplot
+    plt.bar(range(X_treinamento.shape[1]), importances[indices])
+
+    # Adiciona as feature names no eixo x-axis
+    plt.xticks(range(X_treinamento.shape[1]), names, rotation = 20, fontsize = 8)
+
+    # Define o título do gráfico
+    plt.title("Importância Preditiva das variáveis")
+    plt.show()
